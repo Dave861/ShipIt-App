319558237a42aa551ceaa8cec2a60192785bd2f9
mihneabondor <mihnea.bondor@icloud.com>
2022-12-27 12:57:53 +0200

    Added color set

---
+++ ShipIt/WelcomeView.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeView.swift
+//  ShipIt
+//
+//  Created by Mihnea on 12/27/22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "box.truck.fill")
+                
+        }
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
