7d5c843b53a4b49e8510ee87be4e8d043c91ef3d
David Retegan <david.retegan@icloud.com>
2022-12-27 12:08:07 +0200

    Initial Commit

---
+++ ShipIt/ShipItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShipItApp.swift
+//  ShipIt
+//
+//  Created by David Retegan on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShipItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
